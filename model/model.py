d3c8f6e44f045ecbfe2e4afa4c590b5d2233b77f
Hui <52814694+suhui0803@users.noreply.github.com>
2026-03-18 11:23:49 +0800

    Update model.py

--- model/model.py
+++ model/model.py
@@ -5,8 +5,8 @@ def DPMODEL(symbol_features: int, embedding_layers: list, Rcut: float, fitting_l
     """
     wgdpmodel
     """
-    lnorm=False
+    lnorm=True
     norm_type='layer' #'batch' 'layer'
-    bias=True
+    bias=False
     model = DPNET(symbol_features=symbol_features, embedding_layers=embedding_layers, Rcut=Rcut, fitting_layers=fitting_layers, lnorm=lnorm, norm_type=norm_type, bias=bias)
     return model
