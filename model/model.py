e629a5ad65317ec633b74f0d4c0f786606a9a55b
Hui <52814694+suhui0803@users.noreply.github.com>
2026-03-18 11:23:13 +0800

    Update model.py

--- model/model.py
+++ model/model.py
@@ -1,14 +1,12 @@
 import torch
 from .dpnet import DPNET
 
-def DPMODEL(symbol_features: int, embedding_layers: list, Rcut: float, fitting_layers: list, device:None):
+def DPMODEL(symbol_features: int, embedding_layers: list, Rcut: float, fitting_layers: list):
     """
-    dpmodel
+    wgdpmodel
     """
-    lnorm=True
+    lnorm=False
     norm_type='layer' #'batch' 'layer'
-    bias=False
-    if device == None:
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = DPNET(symbol_features=symbol_features, embedding_layers=embedding_layers, Rcut=Rcut, fitting_layers=fitting_layers, lnorm=lnorm, norm_type=norm_type, bias=bias, device=device)
+    bias=True
+    model = DPNET(symbol_features=symbol_features, embedding_layers=embedding_layers, Rcut=Rcut, fitting_layers=fitting_layers, lnorm=lnorm, norm_type=norm_type, bias=bias)
     return model
